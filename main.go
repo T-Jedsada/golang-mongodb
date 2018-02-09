5d60e6bbfb5531f842a0b72fa91cca85f06ad7e8
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-09 16:35:57 +0700

    add crud db

--- main.go
+++ main.go
@@ -10,21 +10,51 @@ import (
 )
 
 func main() {
-	db, err := mgo.Dial("localhost")
+	session, err := mgo.Dial("localhost")
 	if err != nil {
-		log.Fatal("cannot dial mongo", err)
+		log.Fatal("Could not connect to mongo: ", err.Error())
 	}
-	defer db.Close()
-	c := db.DB("test").C("employee")
+	defer session.Close()
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	var employee []model.Employee
-
-	c.Find(bson.M{}).All(&employee)
-
-	for index, item := range employee {
-		fmt.Printf("index at %d : %s\n", index, item)
+	employee := getEmployeeByID(session, "9")
+	if employee == nil {
+		fmt.Println("Could not get employee")
 	}
+	fmt.Println(employee)
+
+	employeies := getAllEmployee(session)
+	if len(employeies) == 0 {
+		fmt.Println("Not have any Employee")
+	} else {
+		for index, item := range employeies {
+			fmt.Printf("index at %d : %s\n", index, item)
+		}
+	}
+}
+
+func getEmployeeByID(s *mgo.Session, employeeID string) (employee *model.Employee) {
+	session := s.Copy()
+	defer session.Close()
+	c := session.DB("test").C("employee")
+	c.Find(bson.M{"id": employeeID}).One(&employee)
+	return employee
+}
+
+func getAllEmployee(s *mgo.Session) (employies []model.Employee) {
+	session := s.Copy()
+	defer session.Close()
+	c := session.DB("test").C("employee")
+	c.Find(bson.M{}).All(&employies)
+	return employies
+}
+
+func deleteEmployee(s *mgo.Session, employeeID string) bool {
+	return false
+}
+
+func addEmployee(s *mgo.Session, employee model.Employee) {
+
 }
